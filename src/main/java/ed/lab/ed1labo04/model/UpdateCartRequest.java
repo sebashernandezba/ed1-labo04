02f172c810057bf604f04f1afcb1e6b4e927f0fe
PC1C2024_1105824_Julio Hernández <158302056+JSHB2023@users.noreply.github.com>
2025-05-26 22:48:11 -0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/ed/lab/ed1labo04/controller/CartController.java
    #	src/main/java/ed/lab/ed1labo04/controller/ProductController.java
    #	src/main/java/ed/lab/ed1labo04/entity/CartEntity.java
    #	src/main/java/ed/lab/ed1labo04/entity/ProductEntity.java
    #	src/main/java/ed/lab/ed1labo04/model/CreateCartRequest.java
    #	src/main/java/ed/lab/ed1labo04/model/CreateProductRequest.java
    #	src/main/java/ed/lab/ed1labo04/model/UpdateProductRequest.java
    #	src/main/java/ed/lab/ed1labo04/service/CartService.java
    #	src/main/java/ed/lab/ed1labo04/service/ProductService.java

---
+++ src/main/java/ed/lab/ed1labo04/model/UpdateCartRequest.java
@@ -0,0 +1,22 @@
+package ed.lab.ed1labo04.model;
+
+public class UpdateCartRequest {
+    public int productId;
+    public int quantity;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
